652654a774f721890a5881f561d744d43fc912b3
Ole Kallerud <ole@kallerud.no>
2022-12-05 17:42:20 +0100

    Delete exceptions.py

--- music_tagger/exceptions.py
+++
@@ -1,7 +0,0 @@
-from http.client import responses
-from music_tagger import colors as Color
-
-class HTTPError(Exception):
-    """Raised when http request fails"""
-    def __init__(self, status_code: int, message: str = "HTTP ERROR"):
-        super().__init__(f"{Color.WARNING}{Color.BOLD}{message}: {Color.ENDC}{status_code} {responses.get(status_code)}")
